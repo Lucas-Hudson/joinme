2d9e354f783197d14c5d2a9166d1b863a89204bd
Ali Youssef <aliyoussef34@hotmail.fr>
2019-09-14 12:00:02 +0200

    create venues table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_091916) do
+ActiveRecord::Schema.define(version: 2019_09_14_094420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,20 @@ ActiveRecord::Schema.define(version: 2019_09_14_091916) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.bigint "owner_id"
+    t.string "name"
+    t.text "description"
+    t.integer "min_drink_price"
+    t.string "address"
+    t.string "city"
+    t.string "zip_code"
+    t.boolean "food"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_venues_on_owner_id"
   end
 
 end
