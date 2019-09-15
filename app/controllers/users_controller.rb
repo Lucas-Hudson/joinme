307c3c8b0916ffa28fa69d4e77a837ed143f15ee
Lucas Hudson <lucashudson.contact@gmail.com>
2019-09-15 13:15:00 +0200

    friend pages and add friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,27 @@
 class UsersController < ApplicationController
-    def show
-        @user = User.find(params[:id])
-    end
 
-    def edit
-        @user = current_user
-    end
+  def index
+    # Show all users who aren't the current_user's friends
+    @users = User.all - Array(current_user) - current_user.friends
+  end
 
-    def update
-        @user = current_user
-        @user.update(user_update)
-        redirect_to root_path
-    end
+  def show
+      @user = User.find(params[:id])
+  end
 
-    private
-    def user_update
-        params.require(:user).permit(:first_name, :last_name, :email, :date_of_birth, :description, user: current_user)
-    end
+  def edit
+      @user = current_user
+  end
+
+  def update
+      @user = current_user
+      @user.update(user_update)
+      redirect_to root_path
+  end
+
+  private
+  def user_update
+      params.require(:user).permit(:first_name, :last_name, :email, :date_of_birth, :description, user: current_user)
+  end
 
 end
